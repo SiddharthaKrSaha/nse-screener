be1f12044a096bd90233f2076d9b3958fbb4c1c3
SiddharthaKrSaha <siddharthaineklaghar@gmail.com>
2026-02-03 15:15:51 +0530

    Update nse_symbols.py

--- scanner/nse_symbols.py
+++ scanner/nse_symbols.py
@@ -1,14 +1,7 @@
-# Full NSE stock list (Yahoo Finance format)
-# Suffix .NS is mandatory
-
-NSE_SYMBOLS = ["20MICRONS",
-"21STCENMGM",
-"360ONE",
-"3IINFOLTD",
-"3MINDIA",
-"3PLAND",
-"5PAISA",
-"63MOONS",
-"A2ZINFRA",
-"AAATECH",
+NSE_SYMBOLS = [
+    "RELIANCE",
+    "TCS",
+    "INFY",
+    "HDFCBANK",
+    "ICICIBANK"
 ]
