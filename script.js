c5f9ea4e33025293842637332b4160870c44d189
SiddharthaKrSaha <siddharthaineklaghar@gmail.com>
2026-02-03 15:17:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-let screenerData = [];
-
-const tableBody = document.querySelector("#screener-table tbody");
-
-// Load JSON data
 fetch("results.json")
-    .then(response => response.json())
-    .then(data => {
-        screenerData = data;
-        renderTable();
-    })
-    .catch(error => {
-        console.error("Error loading data:", error);
+  .then(res => res.json())
+  .then(data => {
+    const tbody = document.querySelector("#screener-table tbody");
+    tbody.innerHTML = "";
+
+    data.forEach(item => {
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+        <td>${item.symbol}</td>
+        <td>${item.cmp}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      `;
+
+      tbody.appendChild(row);
     });
-
-// Render table
-function renderTable() {
-    tableBody.innerHTML = "";
-
-    screenerData.forEach(item => {
-        const row = document.createElement("tr");
-
-        row.innerHTML = `
-            <td>${item.symbol}</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>${item.cmp}</td>
-        `;
-
-        tableBody.appendChild(row);
-    });
-}
+  });
