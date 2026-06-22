257934de5a970b65a9d0d67432def8ebad05c9dd
SiddharthaKrSaha <siddharthaineklaghar@gmail.com>
2026-06-23 01:17:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -293,3 +293,17 @@ document.getElementById("analyzeBtn").addEventListener("click", async () => {
   }
 
 });
+
+new TradingView.widget({
+  "autosize": true,
+  "symbol": "NSE:RELIANCE",
+  "interval": "15",
+  "timezone": "Asia/Kolkata",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "container_id": "tradingview_chart"
+});
